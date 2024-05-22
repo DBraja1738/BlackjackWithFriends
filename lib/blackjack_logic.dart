24739fe45c2bbb0d11e658e6016eb595803909ff
DBraja1738 <dragan.brajkovic091@hotmail.com>
2024-05-22 11:41:16 +0200

    add game logic

--- lib/blackjack_logic.dart
+++ lib/blackjack_logic.dart
@@ -39,3 +39,50 @@ class Deck {
     return _cards.removeLast();
   }
 }
+
+class BlackjackGame {
+  Deck _deck = Deck();
+  List<Card> _playerHand = [];
+  List<Card> _dealerHand = [];
+
+  void startGame() {
+    _playerHand = [_deck.drawCard(), _deck.drawCard()];
+    _dealerHand = [_deck.drawCard(), _deck.drawCard()];
+  }
+
+  void playerHit() {
+    _playerHand.add(_deck.drawCard());
+  }
+
+  void dealerHit() {
+    while (_calculateHandValue(_dealerHand) < 17) {
+      _dealerHand.add(_deck.drawCard());
+    }
+  }
+
+  int _calculateHandValue(List<Card> hand) {
+    int value = hand.fold(0, (sum, card) => sum + card.value);
+    int aces = hand.where((card) => card.rank == 'A').length;
+
+    while (value > 21 && aces > 0) {
+      value -= 10;
+      aces--;
+    }
+
+    return value;
+  }
+
+  String checkOutcome() {
+    int playerValue = _calculateHandValue(_playerHand);
+    int dealerValue = _calculateHandValue(_dealerHand);
+
+    if (playerValue > 21) return 'Player Busts!';
+    if (dealerValue > 21) return 'Dealer Busts! Player Wins!';
+    if (playerValue == dealerValue) return 'Push';
+    if (playerValue > dealerValue) return 'Player Wins!';
+    return 'Dealer Wins!';
+  }
+
+  List<Card> get playerHand => _playerHand;
+  List<Card> get dealerHand => _dealerHand;
+}
